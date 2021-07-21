6c89e85c03103761139c92830668ada2e3dc5183
Teodor <teodorchy_kz@abv.bg>
2021-07-21 21:01:36 +0300

    New monitor asus :D

---
+++ src/main/java/bookstore/web/WriteReviewServlet.java
@@ -0,0 +1,8 @@
+package bookstore.web;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet()
+public class WriteReviewServlet extends HttpServlet {
+}
